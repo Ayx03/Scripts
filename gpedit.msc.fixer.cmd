3aa452a081ddd70b92e62d0c2272d1ecf917d772
Aynxul03 <github@imayx.top>
2022-11-23 02:46:52 +0800

    Administrator permission required.

---
+++ gpedit.msc.fixer.cmd
@@ -0,0 +1,9 @@
+@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+pushd "%~dp0"
+dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt
+dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"C:\Windows\servicing\Packages\%%i"
+echo. Done. Press any key to exit or feel free to close this window.
+pause > nul
