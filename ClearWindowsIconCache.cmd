c907586b23c7559c4485a34315e60e131f556c40
ImAyx <GitHub@ImAyx.Pro>
2023-04-13 21:31:31 +0800

    Update ClearWindowsIconCache.cmd

--- ClearWindowsIconCache.cmd
+++ ClearWindowsIconCache.cmd
@@ -1,15 +1,10 @@
-rem 关闭Windows外壳程序explorer
-
+rem 执行后你可能需要把固定在任务栏上的图标取消固定，关闭程序再打开后才会看到效果，重建图标缓存需要一些时间，耐心等待
+rem 强制杀死 Windows 资源管理器
 taskkill /f /im explorer.exe
-
 rem 清理系统图标缓存数据库
-
 attrib -h -s -r "%userprofile%\AppData\Local\IconCache.db"
-
 del /f "%userprofile%\AppData\Local\IconCache.db"
-
 attrib /s /d -h -s -r "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*"
-
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db"
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_96.db"
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_102.db"
@@ -17,12 +12,8 @@ del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_256.db
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_1024.db"
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_idx.db"
 del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_sr.db"
-
 rem 清理 系统托盘记忆的图标
-
 echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v IconStreams
 echo y|reg delete "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v PastIconsStream
-
 rem 重启Windows外壳程序explorer
-
 start explorer
